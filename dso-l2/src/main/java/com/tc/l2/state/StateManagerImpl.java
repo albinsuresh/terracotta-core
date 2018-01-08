a56acb5a670ed9284e538e875d5af8970933c79f
Albin Suresh <albinsuresh@hotmail.com>
2018-01-08 16:23:36 +0530

    Dummy election changes

--- dso-l2/src/main/java/com/tc/l2/state/StateManagerImpl.java
+++ dso-l2/src/main/java/com/tc/l2/state/StateManagerImpl.java
@@ -64,6 +64,7 @@ public class StateManagerImpl implements StateManager {
   private volatile ServerMode               state               = ServerMode.START;
   private ServerMode               startState               = null;
   private final ElectionGate                      elections  = new ElectionGate();
+  private final boolean consistencyPreferred = true;
 
   // Known servers from previous election
   Set<NodeID> prevKnownServers = new HashSet<>();
@@ -523,6 +524,9 @@ public class StateManagerImpl implements StateManager {
         clusterStatePersistor.setDBClean(false);
         throw new TCServerRestartException("Passive only partially synced when active disappeared.  Restarting"); 
       } else if (state != ServerMode.ACTIVE && activeNode.isNull()) {
+        elect = true;
+      } else if (consistencyPreferred && state == ServerMode.ACTIVE) {
+        activeNode = ServerID.NULL_ID;
         elect = true;
       }
     }
